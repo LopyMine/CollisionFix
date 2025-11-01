1ec561e0157eabcfcbd3c1c67515b07f685ba509
LopyMine <razvlekuha300@gmail.com>
2025-11-01 23:31:14 +0300

    1.21.9+ update with mossy stuff

--- src/main/java/net/lopymine/collisionfix/mixin/EntityMixin.java
+++ src/main/java/net/lopymine/collisionfix/mixin/EntityMixin.java
@@ -9,13 +9,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Entity.class)
 public abstract class EntityMixin {
 
-    @Shadow
-    public abstract World getWorld();
+	//? if >=1.21.9 {
+	@Shadow
+	public abstract World getEntityWorld();
+	//?} else {
+	/*@Shadow
+	public abstract World getWorld();
+	*///?}
 
     @Inject(method = "isInsideWall", at = @At("HEAD"), cancellable = true)
     private void isInsideWall(CallbackInfoReturnable<Boolean> cir) {
-        if (this.getWorld().isClient()) {
+        if (this.getCurrentWorld().isClient()) {
             cir.setReturnValue(false);
         }
     }
+
+	@Unique
+	private World getCurrentWorld() {
+		//? if >=1.21.9 {
+		return this.getEntityWorld();
+		//?} else {
+		/*return this.getWorld();
+		*///?}
+	}
 }
--- src/main/java/net/lopymine/collisionfix/mixin/LivingEntityMixin.java
+++ src/main/java/net/lopymine/collisionfix/mixin/LivingEntityMixin.java
@@ -2,7 +2,7 @@ package net.lopymine.collisionfix.mixin;
 
 import net.minecraft.entity.*;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.*;
 
@@ -15,22 +15,31 @@ public abstract class LivingEntityMixin extends Entity {
 
     @Inject(method = "isInsideWall", at = @At("HEAD"), cancellable = true)
     private void isInsideWall(CallbackInfoReturnable<Boolean> cir) {
-        if (this.getWorld().isClient()) {
+	    if (this.getCurrentWorld().isClient()) {
             cir.setReturnValue(false);
         }
     }
 
-    @Inject(method = "pushAway", at = @At("HEAD"), cancellable = true)
+	@Inject(method = "pushAway", at = @At("HEAD"), cancellable = true)
     private void pushAway(CallbackInfo ci) {
-        if (this.getWorld().isClient()) {
+        if (this.getCurrentWorld().isClient()) {
             ci.cancel();
         }
     }
 
     @Inject(method = "pushAwayFrom", at = @At("HEAD"), cancellable = true)
     private void pushAwayFrom(CallbackInfo ci) {
-        if (this.getWorld().isClient()) {
+        if (this.getCurrentWorld().isClient()) {
             ci.cancel();
         }
     }
+
+	@Unique
+	private World getCurrentWorld() {
+		//? if >=1.21.9 {
+		return this.getEntityWorld();
+		//?} else {
+		/*return this.getWorld();
+		 *///?}
+	}
 }
